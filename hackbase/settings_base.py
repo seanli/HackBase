799f81daa08d167eef8526de60876d141a0506f3
Sean Li <lishang106@gmail.com>
2013-06-24 18:38:32 -0400

    Added csrf context processor.

--- hackbase/settings_base.py
+++ hackbase/settings_base.py
@@ -83,6 +83,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django.core.context_processors.request',
+    'django.core.context_processors.csrf',
     'django.contrib.messages.context_processors.messages',
     'core.context_processors.settings',
 )
